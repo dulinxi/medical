efa94dd73ece30f6534e0540159e9f363cae83dd
杜郡 <95609658@qq.com>
2019-12-11 09:03:07 +0800

    build(webpack): update webpack script

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,8 +1,40 @@
 const path = require('path');
-module.exports = {
+const webpack = require('webpack');
+const pkg = require('../package.json');
+
+const banner = new webpack.BannerPlugin({
+    banner: [
+        `Medicaljs v${pkg.version}`,
+        // 'https://wjsh.com/',
+        'Copyright (c) 2019, Du Jun'
+    ].join('\n'),
+    entryOnly: true,
+});
+const baseConfig = {
+    mode: 'development',
+    context: path.resolve(__dirname, '..'),
+    
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'medicaljs.js'
+        filename: 'medical.js',
+        library: 'Medical',
+        libraryExport: 'default',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname,'../dist')
+    },
+    plugins: [
+        banner
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, '../dist'),
+        hot: false,
+        port: process.env.npm_package_config_ports_webpack,
+        stats: 'minimal',
+        disableHostCheck: true
     }
+};
+
+
+module.exports = () => {
+    return baseConfig;
 }
