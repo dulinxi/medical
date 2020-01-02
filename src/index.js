407c529b02b1cd53bd62affeacd5ebc905486b1f
杜郡 <95609658@qq.com>
2020-01-02 15:49:53 +0800

    feat: initialize the timeline chat

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import * as d3 from 'd3';
+export default class Medical {
+  constructor() {
+    this.version = '1.0';
+    this.timeline = Timeline;
+  }
+}
 
-class Medical {
-    constructor(){
-        
-    }
+class Timeline extends Medical {
+  constructor() {
+    super();
+  }
+  // initialize the timeline
+  init() {}
+  // draw the wireframe of the timeline
+  drawFrame() {
+    console.log(`timeline start ${this.version}`);
+  }
+  // update the timeline when the data change
+  update() {}
 }
