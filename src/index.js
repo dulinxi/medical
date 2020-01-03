afa820d02e74637ef0e558dcec301f616e3e2a5e
杜郡 <95609658@qq.com>
2020-01-03 11:07:07 +0800

    feat: draw frame grid

--- src/index.js
+++ src/index.js
@@ -7,13 +7,60 @@ export default class Medical {
 }
 
 class Timeline extends Medical {
-  constructor() {
+  constructor(config = {}) {
     super();
+    this.layout = ['date', 'indays', 'treatDays', 'noon'];
+    this.config = {
+      width: 300,
+      height: 300,
+      ...config
+    };
+    this.timeline = null;
+    this.timeline = d3
+      .select(this.config.el)
+      .append('svg')
+      .attr('width', this.config.width + 20)
+      .attr('height', this.config.height + 20);
+    this.drawFrame();
   }
   // initialize the timeline
   init() {}
   // draw the wireframe of the timeline
   drawFrame() {
+    const frame = this.timeline.append('g').attr('class', 'frame');
+    const _that = this;
+    frame
+      .selectAll('.vlines')
+      .data(d3.range(51))
+      .enter()
+      .append('line')
+      .attr('class', 'vlines')
+      .attr('x1', function(d, i) {
+        return i * 10;
+      })
+      .attr('y1', 10)
+      .attr('x2', function(d, i) {
+        return i * 10;
+      })
+      .attr('y2', function(d, i) {
+        return _that.config.height;
+      });
+    frame
+      .selectAll('.hlines')
+      .data(d3.range(51))
+      .enter()
+      .append('line')
+      .attr('class', 'hlines')
+      .attr('x1', 10)
+      .attr('y1', function(d, i) {
+        return i * 10;
+      })
+      .attr('x2', function(d, i) {
+        return _that.config.width;
+      })
+      .attr('y2', function(d, i) {
+        return i * 10;
+      });
     console.log(`timeline start ${this.version}`);
   }
   // update the timeline when the data change
